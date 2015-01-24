57e7be4d6335e247c4f0fb31784ad1b8d2d12b67
kunal-patil <kunal.patil@asymmetrix.co.in>
2015-01-24 15:40:06 +0530

    Added the function body for both the functions in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following 2 functions will help us to cache the inverse of a matrix. 
+## Calculating the inverse of a fairly large matrix is a resource intensive process, 
+##   hence if we are going to need the inverse of the same matrix over and over again in the same session, its advised to cache it.
 
-## Write a short comment describing this function
-
+## The following function creates an interface to a matrix.
+## Returns a list consisting of functions which help to set & get the matrix and its Inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+	m_inv <- NULL
+	set <- function(y)
+	{
+		x <<- y
+		m_inv <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(inv, ...) m_inv <<- inv
+	getInverse <- function() m_inv
+	list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## CacheSolve function checks if the Inverse has already been calculated for the underlying matrix, if not, calculates the Inverse and caches it for future reference.
+##  Further calls to this function with the same argument(matrix interface created using above makeMatrix function) simply returns the cached value of the Inverse. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getInverse()
+	if(!is.null(inv))
+	{
+		message("returning cached Inverse")
+		return (inv)
+	}
+	temp = x$get()
+	inv <- solve(temp)
+	x$setInverse(inv)
+	inv
 }
